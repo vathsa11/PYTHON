7eb77b3d28cc5c2958de921be35b7325c28a4c8d
vathsa11 <64072823+vathsa11@users.noreply.github.com>
2020-08-24 19:05:04 +0530

    Update extension of file.py

--- extension of file.py
+++ extension of file.py
@@ -1,3 +1,14 @@
 name=input("Input the Filename: ")
 ext=name.split(".")
-print("The extension of the file is : "+ext[-1])
+if ext[-1]=="py":
+    print("The extension of the file is : python")
+elif ext[-1]=="cpp":
+    print("The extension of the file is : c++")
+elif ext[-1]=="doc":
+    print("The extension of the file is : word")
+elif ext[-1]=="txt":
+    print("The extension of the file is : text")
+elif ext[-1]=="exe":
+    print("The extension of the file is : executable")
+else:
+    print("The extension of the file is : ",ext[-1])
