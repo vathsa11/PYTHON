656121b3274cc85f12293a31c64b7869d54962a1
vathsa11 <64072823+vathsa11@users.noreply.github.com>
2020-08-24 19:14:47 +0530

    Program-4
    to print all positive numbers in a range

---
+++ positive numbers.py
@@ -0,0 +1,11 @@
+list1=[12,-7,5,64,-14]
+list2=[12,14,-95,3]
+print("list1 = ",list1)
+for i in list1:
+    if i>0:
+        print(i,end=', ')
+        
+print("\nlist2 = ",list2)
+for i in list2:
+    if i>0:
+        print(i,end=', ')
